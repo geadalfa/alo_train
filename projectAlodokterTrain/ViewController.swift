d9b455baf5bbeaf10b8c4fba785bbc5d92bc8c84
Geadalfa Giyanda <ggeadalfa@gmail.com>
2021-11-27 10:46:35 +0700

    Initial Commit

---
+++ projectAlodokterTrain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  projectAlodokterTrain
+//
+//  Created by Geadalfa Giyanda on 27/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
